ea9e1b70029487177d44117167ddb5eab32d4a86
nuttyCream <issanutty@gmail.com>
2025-03-04 11:12:37 -0800

    feat: add niri cfg

--- niri.mod.nix
+++ niri.mod.nix
@@ -8,7 +8,7 @@
     package = pkgs.niri-unstable;
   };
   nixpkgs.overlays = [inputs.niri.overlays.niri];
+  home-manager.users.j.programs.niri.config = builtins.readFile ./configs/niri.kdl; 
 
-  programs.niri.settings = {
-  };
 }
+
