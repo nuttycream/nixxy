31e26841f6762f87c5f3aa706b95e84dd54e6c22
nuttyCream <issanutty@gmail.com>
2025-03-03 23:37:00 -0800

    feat: pavucontrol

--- conf.nix
+++ conf.nix
@@ -107,6 +107,7 @@
     xwayland-satellite
     swaybg
     brightnessctl
+    pavucontrol
     ((pkgs.ffmpeg-full.override { withUnfree = true; }).overrideAttrs (_: { doCheck = false; }))
   ];
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
