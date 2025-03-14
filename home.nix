6ce9290214996dab403c2e5ca528f362fbb95b9f
nuttyCream <issanutty@gmail.com>
2025-03-14 14:50:41 -0700

    feat: add cyberdream.nvim colors to foot

--- home.nix
+++ home.nix
@@ -58,8 +58,26 @@
     enable = true;
     settings = {
       main.font = "Geist Mono:size=11";
+      cursor.color = "16181a ffffff";
       colors = {
         alpha = 0;
+        foreground = "ffffff";
+        regular0 = "16181a";
+        regular1 = "ff6e5e";
+        regular2 = "5eff6c";
+        regular3 = "f1ff5e";
+        regular4 = "5ea1ff";
+        regular5 = "bd5eff";
+        regular6 = "5ef1ff";
+        regular7 = "ffffff";
+        bright0 = "3c4048";
+        bright1 = "ff6e5e";
+        bright2 = "5eff6c";
+        bright3 = "f1ff5e";
+        bright4 = "5ea1ff";
+        bright5 = "bd5eff";
+        bright6 = "5ef1ff";
+        bright7 = "ffffff";
       };
     };
   };
