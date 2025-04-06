76b4a510f8ce1f2562de3b70462e28467de44d0b
nuttyCream <issanutty@gmail.com>
2025-04-06 11:54:05 -0700

    feat: add some virtualisation

--- conf.nix
+++ conf.nix
@@ -132,6 +132,11 @@
     xdgOpenUsePortal = true;
   };
 
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = ["j"];
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
