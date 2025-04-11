1aa5ce11705fdaf5e0dafe51aca452d6a303c826
nuttyCream <issanutty@gmail.com>
2025-04-10 20:51:50 -0700

    fix: temp disable xone controller

--- desky.hardware.nix
+++ desky.hardware.nix
@@ -21,7 +21,7 @@
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
-  hardware.xone.enable = true;
+  # hardware.xone.enable = true;
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/65a9cc2a-e2c8-48c9-af1e-3d9b9ede34cc";
