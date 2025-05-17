08e15726c5525da35095f5362f09d20ab3ea1d4c
nuttyCream <issanutty@gmail.com>
2025-05-17 01:21:35 -0700

    feat: graphics supp

--- conf.nix
+++ conf.nix
@@ -63,6 +63,7 @@
       "networkmanager"
       "wheel"
       "video"
+      "max"
     ];
   };
 
@@ -93,10 +94,15 @@
 
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      rocmPackages.clr.icd
+      amdvlk
+    ];
   };
 
   # services.xserver.videoDrivers = [“nvidia”];
-  services.xserver.videoDrivers = ["amdgpu"];
+  # services.xserver.videoDrivers = ["amdgpu"];
 
   environment.systemPackages = with pkgs; [
     wget
