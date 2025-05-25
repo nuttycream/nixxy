bbc5391342cd2affe9edce2afbd9956da0b60bdb
nuttyCream <issanutty@gmail.com>
2025-05-24 21:52:58 -0700

    feat: addtl configuration stuff

--- conf.mod.nix
+++ conf.mod.nix
@@ -19,6 +19,22 @@ in {
         experimental-features = ["nix-command" "flakes"];
         trusted-users = ["root" "j"];
       };
+      security.rtkit.enable = true;
+
+      time.timeZone = "America/Los_Angeles";
+
+      i18n.defaultLocale = "en_US.UTF-8";
+      i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+      };
     }
     garbage-collection-module
   ];
