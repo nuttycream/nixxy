cb27d89345d73697ad39f3effd23da247c3e2113
nuttyCream <issanutty@gmail.com>
2025-05-24 21:47:53 -0700

    feat: podman, docker, distrobox

---
+++ virtualization.mod.nix
@@ -0,0 +1,19 @@
+{...}: {
+  universal.modules = [
+    ({pkgs, ...}: {
+      environment.systemPackages = with pkgs; [
+        distrobox
+      ];
+      programs.virt-manager.enable = true;
+      users.groups.libvirtd.members = ["j"];
+      virtualisation = {
+        libvirtd.enable = true;
+        spiceUSBRedirection.enable = true;
+        podman = {
+          enable = true;
+          dockerCompat = true;
+        };
+      };
+    })
+  ];
+}
