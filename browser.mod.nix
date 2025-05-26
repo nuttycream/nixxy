daf9605268db24effe461a4b1e0120640adfdc1b
nuttyCream <issanutty@gmail.com>
2025-05-26 12:40:19 -0700

    feat: floorp -> firefox

--- browser.mod.nix
+++ browser.mod.nix
@@ -1,29 +1,59 @@
-{firefox-addons, ...}: {
+{
+  firefox-addons,
+  firefox-nightly,
+  ...
+}: {
+  # get the nightly version
+  personal.modules = [
+    ({pkgs, ...}: {
+      environment.systemPackages = [
+        firefox-nightly.packages.${pkgs.system}.firefox-nightly-bin
+      ];
+    })
+  ];
+
   personal.home_modules = [
-    {
-      programs.floorp = {
+    ({...}: {
+      programs.firefox = {
         enable = true;
+        profiles = {
+          "j" = {
+            id = 0;
+            isDefault = true;
 
-        policies = {
-          FirefoxHome = {
-            AutofillAddressEnabled = false;
-            AutofillCreditCardEnabled = false;
-            SponsoredTopSites = false;
-            SponsoredPocket = false;
-            DisableTelemetry = true;
-          };
-        };
-
-        profiles.j = {
-          extensions = {
-            force = true;
-            packages = with firefox-addons.packages.x86_64-linux; [
-              ublock-origin
-              bitwarden
-            ];
+            search.engines = {
+              "Nix Packages" = {
+                urls = [
+                  {
+                    template = "https://search.nixos.org/packages";
+                    params = [
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+                definedAliases = ["@np"];
+              };
+              "Nix Options" = {
+                definedAliases = ["@no"];
+                urls = [
+                  {
+                    template = "https://search.nixos.org/options";
+                    params = [
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+              };
+            };
           };
         };
       };
-    }
+    })
   ];
 }
--- flake.nix
+++ flake.nix
@@ -21,6 +21,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    firefox-nightly = {
+      url = "github:nix-community/flake-firefox-nightly";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     niri = {
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
