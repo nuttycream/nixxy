5fff5ecb2989b701be1de7c09a241781fc292662
nuttyCream <issanutty@gmail.com>
2025-05-26 16:11:35 -0700

    fix: broken home-manager backup system, disabling firefox home mgment

--- browser.mod.nix
+++ browser.mod.nix
@@ -13,9 +13,21 @@
   ];
 
   personal.home_modules = [
-    ({...}: {
+    ({pkgs, ...}: {
+      home.packages = [
+        pkgs.firefox
+      ];
+
+      # this shit keeps nagging at me about the back up file being clobbered
+      # i dont want home-manager to manage it anymore
+      # fucking christ
+      # relevant issue:
+      # https://github.com/nix-community/home-manager/issues/4199
+      /*
       programs.firefox = {
         enable = true;
+
+
         profiles = {
           "j" = {
             id = 0;
@@ -54,6 +66,7 @@
           };
         };
       };
+      */
     })
   ];
 }
