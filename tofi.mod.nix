9d5ff6990b62ac81edbb05c0d2adb963785af353
nuttyCream <issanutty@gmail.com>
2025-05-27 14:05:17 -0700

    feat: transparent launcher

--- tofi.mod.nix
+++ tofi.mod.nix
@@ -7,7 +7,7 @@
           font = "IBM Plex Serif";
           height = "100%";
           width = "100%";
-          background-color = "#000000";
+          background-color = "#00000080";
           border-width = 0;
           num-results = 10;
           outline-width = 0;
