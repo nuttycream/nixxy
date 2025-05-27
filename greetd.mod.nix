7d61df579383d41d407ef73ae73abdee41be3723
nuttyCream <issanutty@gmail.com>
2025-05-26 22:19:58 -0700

    feat: ly -> greetd, ly was giving me problems with niri, so fuck it all

---
+++ greetd.mod.nix
@@ -0,0 +1,23 @@
+{...}: {
+  personal.modules = [
+    ({
+      pkgs,
+      config,
+      ...
+    }: {
+      #services.xserver.displayManager.startx.enable = true;
+
+      services.greetd = {
+        enable = true;
+        settings = {
+          default_session = {
+            command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
+            user = "j";
+          };
+        };
+      };
+
+      security.pam.services.greetd.enableGnomeKeyring = true;
+    })
+  ];
+}
