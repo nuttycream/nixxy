937f6eff4a07cf50714de8006ebd00772bba810e
nuttyCream <issanutty@gmail.com>
2025-08-06 15:57:25 -0700

    feat: even more completions!

--- shell.mod.nix
+++ shell.mod.nix
@@ -6,6 +6,7 @@
       environment.systemPackages = [
         pkgs.nix-output-monitor
         pkgs.nvd
+        pkgs.fish
       ];
 
       services.nixos-cli = {
@@ -44,12 +45,52 @@
             nixos _carapace nushell ...$spans | from json
           }
 
+          let carapace_completer = {|spans: list<string>|
+            carapace $spans.0 nushell ...$spans
+            | from json
+            | if ($in | default [] | where value =~ '^-.*ERR$' | is-empty) { $in } else { null }
+          }
+
+          let fish_completer = {|spans|
+            fish --command $"complete '--do-complete=($spans | str replace --all "'" "\\'" | str join ' ')'"
+              | from tsv --flexible --noheaders --no-infer
+              | rename value description
+              | update value {|row|
+                let value = $row.value
+                  let need_quote = ['\' ',' '[' ']' '(' ')' ' ' '\t' "'" '"' "`"] | any {$in in $value}
+                if ($need_quote and ($value | path exists)) {
+                  let expanded_path = if ($value starts-with ~) {$value | path expand --no-symlink} else {$value}
+                  $'"($expanded_path | str replace --all "\"" "\\\"")"'
+                } else {$value}
+              }
+          }
+
+          let completers = {|spans|
+            let expanded_alias = scope aliases
+              | where name == $spans.0
+              | get -o 0.expansion
+
+            let spans = if $expanded_alias != null {
+              $spans
+                | skip 1
+                | prepend ($expanded_alias | split row ' ' | take 1)
+            } else {
+                $spans
+            }
+
+            match $spans.0 {
+              git => $fish_completer
+              nixos => $nixos_completer
+              _ => $carapace_completer
+            } | do $in $spans
+          }
+
           # https://www.nushell.sh/cookbook/external_completers.html
           $env.config = {
             completions: {
               external: {
                 enable: true
-                completer: $nixos_completer
+                completer: $completers
               }
             }
           }
