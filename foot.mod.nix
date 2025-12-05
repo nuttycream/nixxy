6a1351167c001c28669ffc5f182a620abf80612c
nuttyCream <issanutty@gmail.com>
2025-12-05 12:50:21 -0800

    style(foot): Update foot terminal font to CommitMono

--- foot.mod.nix
+++ foot.mod.nix
@@ -40,7 +40,7 @@
       programs.foot = {
         enable = true;
         settings = {
-          main.font = "IBM Plex Mono: size=12";
+          main.font = "CommitMono: size=12";
         };
       };
     }
